41339d1b92df6332013906b0f70081aeb5bf0194
ruslan <ruslan.naryzhniy@gmail.com>
2017-07-20 01:57:43 +0300

    add tests

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -24,4 +24,38 @@ class DefaultControllerTest extends WebTestCase
         // Test that response is not empty
         $this->assertNotEmpty($client->getResponse()->getContent());
     }
+
+    public function testHazardous()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/neo/hazardous');
+
+        $response = $client->getResponse();
+        // Test if response is OK
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        // Test that a string is a valid JSON string
+        $this->assertJson($response->getContent());
+        // Test if Content-Type is valid application/json
+        $this->assertSame('application/json', $response->headers->get('Content-Type'));
+        // Test that response is not empty
+        $this->assertNotEmpty($client->getResponse()->getContent());
+    }
+
+    public function testFastest()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/neo/fastest');
+
+        $response = $client->getResponse();
+        // Test if response is OK
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        // Test that a string is a valid JSON string
+        $this->assertJson($response->getContent());
+        // Test if Content-Type is valid application/json
+        $this->assertSame('application/json', $response->headers->get('Content-Type'));
+        // Test that response is not empty
+        $this->assertNotEmpty($client->getResponse()->getContent());
+    }
 }
